05f5704d4b3eb90aaaac1eb9b561701066a16653
kiboma2021 <omayioben@gmail.com>
2022-10-20 15:49:10 +0300

    Correct linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,16 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments, dependent: :delete_all
-    has_many :likes, dependent: :delete_all
+  belongs_to :author, class_name: 'User'
+  has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
 
-    after_create :update_posts_count
-    def update_posts_count
-      author.posts_counter = 0 unless author.posts_counter?
-      author.posts_counter += 1
-      author.save
-    end
-  
-    def most_recent_comments
-      Comment.includes(:author).where(post: self).order(created_at: :desc).limit(5)
-    end
+  after_create :update_posts_count
+  def update_posts_count
+    author.posts_counter = 0 unless author.posts_counter?
+    author.posts_counter += 1
+    author.save
+  end
+
+  def most_recent_comments
+    Comment.includes(:author).where(post: self).order(created_at: :desc).limit(5)
+  end
 end
-  
