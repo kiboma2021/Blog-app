597e148668c50b3866715bd8728074f60e73ae9f
kiboma <omayioben@gmail.com>
2022-10-25 22:06:31 +0300

    Add index and show actions in both users and post controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  def likes; end
+  def index
+    @users = User.all
+  end
 
-  def posts; end
-
-  def comments; end
+  def show
+    @user = User.find(params[:id])
 end
