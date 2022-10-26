0f0f36d89fbfdb90ab2472f7f14526b5d8e16146
kiboma2021 <omayioben@gmail.com>
2022-10-26 20:42:12 +0300

    Update post controller and post views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author: @user)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def show
