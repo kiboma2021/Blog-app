293fe34c914e4d3f0ee7700d7041890655cb6fbd
kiboma <omayioben@gmail.com>
2022-10-27 09:05:31 +0300

    Update roots

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create]
       resources :likes, only: [:create]
+    end
   end
-
 end
 
