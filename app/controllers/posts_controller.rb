9887c24c4708ec20c09d956ca96224518a071eeb
kiboma <omayioben@gmail.com>
2022-10-27 08:14:29 +0300

    Create Post and Comment methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,33 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comms = @post.comments.includes(:author)
   end
+
+  def new
+    @post = Post.new
+    @user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    author = current_user
+    post = Post.new(params.require(:post).permit(:comments_counter, :likes_counter, :author, :title, :text))
+    post.likes_counter = 0
+    post.comments_counter = 0
+    post.author = author
+
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'Post created successfully!'
+          redirect_to user_url(author)
+        else
+          flash.now[:error] = 'Error: Post could not be created.'
+          render :new, locals: { post: }, status: 422
+        end
+      end
+    end
+  end
+
 end
