312c1bc6967723de3fcb993a329c7c3b1137868b
kiboma2021 <omayioben@gmail.com>
2022-11-01 20:39:46 +0300

    Test index page

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'users #index', type: :feature do
+    before :each do
+      user1 = User.create(
+        name: 'Ben',
+        photo: 'https://www.pexels.com/photo/cottages-in-the-middle-of-beach-753626/',
+        bio: "I'm finding it hard coping with ruby on rails",
+        posts_counter: 0
+      )
+      user2 = User.create(
+        name: 'Samson',
+        photo: 'https://www.pexels.com/photo/a-rocky-shore-under-clear-blue-sky-8821913/',
+        bio: "I'm love coding using python",
+        posts_counter: 0
+      )
+      @users = [user1, user2]
+      visit '/users'
+    end
+  
+    it 'displays all users cards' do
+      @users.each do |user|
+        expect(page).to have_content(user.name)
+        expect(page).to have_css("img[src*='#{user.photo}']")
+        expect(page).to have_content("Number of posts: #{user.posts_counter}")
+      end
+    end
+  
+    it 'changes to user profile after clicking on a card' do
+      click_link(@users.first.name.to_s)
+      expect(page).to have_content('Profile')
+    end
+  end
