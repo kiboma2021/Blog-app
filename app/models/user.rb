aeb9ea3e57a0571a85bf3ecc967137fedc3d7694
kiboma2021 <omayioben@gmail.com>
2022-11-03 21:56:17 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :user_id, dependent: :destroy
   has_many :likes, foreign_key: :user_id, dependent: :destroy
 
-  #validates :name, presence: true
-  #validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_post
     Post.where(author: self).order(created_at: :desc).limit(3)
   end
 end
-
