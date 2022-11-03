4a593752bc7a858c548fce386e5cd61a49635ab2
kiboma2021 <omayioben@gmail.com>
2022-11-03 23:23:13 +0300

    defining ability

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :manage, :all if user.role == 'admin'
+
+    can :delete_post, Post, author: user
+    can :delete_comment, Comment, author: user
+  end
+end
