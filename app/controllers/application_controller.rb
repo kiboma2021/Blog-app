f99a5bbdd7abb2e2038ea84be81be751216d57a9
kiboma2021 <omayioben@gmail.com>
2022-11-04 23:35:32 +0300

    Create authorization request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,20 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_request, if: :json_request
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.call(request.headers).result
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
+
+  def json_request
+    request.format.json?
+  end
+
   protected
 
   def configure_permitted_parameters
